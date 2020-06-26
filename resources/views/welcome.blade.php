a8010e8ba9f9956e4ce884551aed5565fd1ec1aa
r4tu1 <ratul4g@gmail.com>
2020-06-26 14:42:05 +0600

    text change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    www link paste 
+                    Website 
                 </div>
 
                 <div class="links">
